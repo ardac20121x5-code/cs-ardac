6fa779e868c4c14c675c257e94739c50cd25920d
ardac20121x5-code <ardac2012.1x5@gmail.com>
2026-08-14 07:36:45 +0300

    Initial gradle configuration

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+version = "1.0.0"
+
+cloudstream {
+    language = "tr"
+    authors = listOf("ardac20121x5-code")
+    description = "Dizipal ve HDFilmchennemini'den film ve dizi izle"
+    apiVersion = 1
+}
